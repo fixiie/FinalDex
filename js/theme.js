7c51221f18cb582fef5d3de048cfc66aaf53ed6d
fixiie <warzhl0l@hotmail.com>
2021-12-10 03:44:25 +0100

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,22 +1,24 @@
-const toggleSwitch = document.querySelector('#theme input[type="checkbox"]');
-const currentTheme = localStorage.getItem('finaldex-theme');
+function theme() {
+    const toggleSwitch = document.querySelector('#theme input[type="checkbox"]');
+    const currentTheme = localStorage.getItem('finaldex-theme');
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
+        if (currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+        }
     }
-}
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('finaldex-theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('finaldex-theme', 'light');
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('finaldex-theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('finaldex-theme', 'light');
+        }
     }
-}
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+    toggleSwitch.addEventListener('change', switchTheme, false);
+}
