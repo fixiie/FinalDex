4b276db435cb14a6c046982996e6910cc9fbc32d
fixiie <warzhl0l@hotmail.com>
2021-12-10 03:43:51 +0100

    Update create-reset.js

--- js/create-reset.js
+++ js/create-reset.js
@@ -4,21 +4,23 @@ var resetDivContent = document.createElement("div");
 var resetAside1 = document.createElement("aside");
 var resetAside1Text = document.createElement("p");
 var resetAside2 = document.createElement("aside");
-var resetAside2ButtonYes = document.createElement("button");
-var resetAside2ButtonNo = document.createElement("button");
+var resetAside2ButtonCheck = document.createElement("button");
+var resetAside2ButtonUncheck = document.createElement("button");
 
 
-resetDiv.setAttribute("id","resetcheck-modal");
-resetDiv.classList.add("resetcheck-modal-outer");
-resetDivOverlay.classList.add("resetcheck-modal-overlay");
-resetDivContent.classList.add("resetcheck-modal");
+resetDiv.setAttribute("id","reset-modal");
+resetDiv.classList.add("reset-modal-outer");
+resetDivOverlay.classList.add("reset-modal-overlay");
+resetDivContent.classList.add("reset-modal");
 resetAside1.setAttribute("id","reset-aside1");
-resetAside1Text.innerText = "Are you sure you want to reset the Pokédex?";
+resetAside1Text.innerText = "Select Option:";
 resetAside2.setAttribute("id","reset-aside2");
-resetAside2ButtonYes.classList.add("resetcheck-modal-close2");
-resetAside2ButtonYes.innerText = "Yes";
-resetAside2ButtonNo.classList.add("resetcheck-modal-close1");
-resetAside2ButtonNo.innerText = "No";
+resetAside2ButtonCheck.classList.add("reset-modal-close1");
+resetAside2ButtonCheck.innerText = "Check All";
+resetAside2ButtonCheck.setAttribute("title","Applies to current filters only");
+resetAside2ButtonUncheck.classList.add("reset-modal-close2");
+resetAside2ButtonUncheck.innerText = "Uncheck All";
+resetAside2ButtonUncheck.setAttribute("title","Applies to current filters only");
 
 
 document.getElementById("reset").appendChild(resetDiv);
@@ -27,5 +29,5 @@ resetDiv.appendChild(resetDivContent);
 resetDivContent.appendChild(resetAside1);
 resetAside1.appendChild(resetAside1Text);
 resetDivContent.appendChild(resetAside2);
-resetAside2.appendChild(resetAside2ButtonYes);
-resetAside2.appendChild(resetAside2ButtonNo);
+resetAside2.appendChild(resetAside2ButtonCheck);
+resetAside2.appendChild(resetAside2ButtonUncheck);
