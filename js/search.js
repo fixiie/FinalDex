d227c63690dbc949534644fb0f9d2da0e478afd8
fixiie <warzhl0l@hotmail.com>
2021-12-10 03:44:19 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -15,31 +15,126 @@ $('#searchbar').on('keyup', function() {
     } else {
         filterItems.removeClass('hidden');
     }
+    count();
 });
 
 
 
+if (Generation >= 3) {
+    $('#ability-options-search input').on('keyup', function() {
+        var searchVal = $(this).val();
+        var filterItems = $('#ability-options label');
 
-$('#ability-options-search input').on('keyup', function() {
-    var searchVal = $(this).val();
-    var filterItems = $('#ability-options label');
-
-    if (searchVal != '') {
-        filterItems.addClass('hidden');
-        $('[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
-    } else {
-        filterItems.removeClass('hidden');
-    }
-});
+        if (searchVal != '') {
+            filterItems.addClass('hidden');
+            $('#ability-options label[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
+        } else {
+            filterItems.removeClass('hidden');
+        }
+    });
+}
 
 $('#move-options-search input').on('keyup', function() {
     var searchVal = $(this).val();
     var filterItems = $('#move-options label');
+    var labels = document.querySelectorAll("#move-options label");
+    this.style.color = "var(--fontDark)";
 
-    if (searchVal != '') {
+    if (searchVal.toLowerCase().includes('type::')) {
         filterItems.addClass('hidden');
-        $('[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
-    } else {
+        $('#move-options label[data-search-type*="' + searchVal.toLowerCase().split("type::")[1].replaceAll(" ","") + '"]').removeClass('hidden');
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('category::')) {
+        filterItems.addClass('hidden');
+        $('#move-options label[data-search-category*="' + searchVal.toLowerCase().split("category::")[1].replaceAll(" ","") + '"]').removeClass('hidden');
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('pp::')) {
+        filterItems.addClass('hidden');
+        $('#move-options label[data-search-pp*="' + searchVal.toLowerCase().split("pp::")[1].replaceAll(" ","") + '"]').removeClass('hidden');
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('power::')) {
+        filterItems.addClass('hidden');
+        $('#move-options label[data-search-power*="' + searchVal.toLowerCase().split("power::")[1].replaceAll(" ","") + '"]').removeClass('hidden');
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('accuracy::')) {
+        filterItems.addClass('hidden');
+        $('#move-options label[data-search-accuracy*="' + searchVal.toLowerCase().split("accuracy::")[1].replaceAll(" ","") + '"]').removeClass('hidden');
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('contact::')) {
+        filterItems.addClass('hidden');
+        $('#move-options label[data-search-contact*="' + searchVal.toLowerCase().split("contact::")[1].replaceAll(" ","") + '"]').removeClass('hidden');
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('range::')) {
+        filterItems.addClass('hidden');
+        $('#move-options label[data-search-range*="' + searchVal.toLowerCase().split("range::")[1].replaceAll(" ","") + '"]').removeClass('hidden');
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('pp:>')) {
+        for (var i = 0; i < labels.length; i++) {
+            labels[i].classList.add("hidden");
+            if (parseInt(labels[i].getAttribute("data-search-pp")) >= parseInt(searchVal.toLowerCase().split("pp:>")[1].replaceAll(" ",""))) {
+                labels[i].classList.remove('hidden');
+            }
+        }
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('pp:<')) {
+        for (var i = 0; i < labels.length; i++) {
+            labels[i].classList.add("hidden");
+            if (parseInt(labels[i].getAttribute("data-search-pp")) <= parseInt(searchVal.toLowerCase().split("pp:<")[1].replaceAll(" ",""))) {
+                labels[i].classList.remove('hidden');
+            }
+        }
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('power:>')) {
+        for (var i = 0; i < labels.length; i++) {
+            labels[i].classList.add("hidden");
+            if (parseInt(labels[i].getAttribute("data-search-power")) >= parseInt(searchVal.toLowerCase().split("power:>")[1].replaceAll(" ",""))) {
+                labels[i].classList.remove('hidden');
+            }
+        }
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('power:<')) {
+        for (var i = 0; i < labels.length; i++) {
+            labels[i].classList.add("hidden");
+            if (parseInt(labels[i].getAttribute("data-search-power")) <= parseInt(searchVal.toLowerCase().split("power:<")[1].replaceAll(" ",""))) {
+                labels[i].classList.remove('hidden');
+            }
+        }
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('accuracy:>')) {
+        for (var i = 0; i < labels.length; i++) {
+            labels[i].classList.add("hidden");
+            if (parseInt(labels[i].getAttribute("data-search-accuracy").replaceAll("%","")) >= parseInt(searchVal.toLowerCase().split("accuracy:>")[1].replaceAll(" ",""))) {
+                labels[i].classList.remove('hidden');
+            }
+        }
+        this.style.color = "var(--colorRed)";
+    }
+    else if (searchVal.toLowerCase().includes('accuracy:<')) {
+        for (var i = 0; i < labels.length; i++) {
+            labels[i].classList.add("hidden");
+            if (parseInt(labels[i].getAttribute("data-search-accuracy").replaceAll("%","")) <= parseInt(searchVal.toLowerCase().split("accuracy:<")[1].replaceAll(" ",""))) {
+                labels[i].classList.remove('hidden');
+            }
+        }
+        this.style.color = "var(--colorRed)";
+    }
+
+    else if (!searchVal == '' && !searchVal.toLowerCase().includes("::")) {
+        filterItems.addClass('hidden');
+        $('#move-options label[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
+    } 
+    else {
         filterItems.removeClass('hidden');
     }
 });
@@ -50,7 +145,7 @@ $('#item-options-search input').on('keyup', function() {
 
     if (searchVal != '') {
         filterItems.addClass('hidden');
-        $('[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
+        $('#item-options label[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
     } else {
         filterItems.removeClass('hidden');
     }
@@ -62,7 +157,7 @@ $('#map-options-search input').on('keyup', function() {
 
     if (searchVal != '') {
         filterItems.addClass('hidden');
-        $('[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
+        $('#map-options label[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
     } else {
         filterItems.removeClass('hidden');
     }
@@ -76,7 +171,7 @@ $('#map-options-search input').on('keyup', function() {
 
     if (searchVal != '') {
         filterItems.addClass('hidden');
-        $('[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
+        $('#map-options label[data-search-name*="' + searchVal.toLowerCase() + '"]').removeClass('hidden');
     } else {
         filterItems.removeClass('hidden');
     }
@@ -90,12 +185,18 @@ $('#map-options-search input').on('keyup', function() {
 document.querySelector("#search-exit").addEventListener("click", exitSearch);
 
 function exitSearch() {
+    var filters = document.querySelectorAll('.filterby input:checked');
     var search = document.getElementById("searchbar");
-    search.value = "";
-    $('#contain div').removeClass('hidden');
-    search.focus();
+
+    if (filters.length == 0) {
+        search.value = "";
+        $('#contain-inner div').removeClass('hidden');
+        search.focus();
+        count();
+    }
 }
 
+if (Generation >= 3) {
 document.querySelector("#ability-search-exit").addEventListener("click", exitAbilitySearch);
 
 function exitAbilitySearch() {
@@ -103,6 +204,7 @@ function exitAbilitySearch() {
     search.value = "";
     $('#ability-options label').removeClass('hidden');
     search.focus();
+}
 }
 
 document.querySelector("#move-search-exit").addEventListener("click", exitMoveSearch);
