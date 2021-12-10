81c0e4c888d3126d2dbcf18cf16418f6c21cb4e0
fixiie <warzhl0l@hotmail.com>
2021-12-10 03:44:01 +0100

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,12 +1,13 @@
-const loaddescription = document.querySelector("#load-description");
-const load = document.querySelector("#load");
 
 
-window.addEventListener("load", function() {
+
+function load() {
+    const loaddescription = document.querySelector("#load-description");
+    const load = document.querySelector("#load");
     document.body.style.overflowY = "unset";
     sleep(1000).then(() => {
         document.documentElement.scrollTop = 0;
         load.className += "hidden";
         document.getElementById('resize').click();
     });
-});
+}
