f674011bcd7f3e79574ae9566ce8be9f80d2ee02
fixiie <warzhl0l@hotmail.com>
2021-12-10 03:43:35 +0100

    Update count.js

--- js/count.js
+++ js/count.js
@@ -1,19 +1,4 @@
 function count() {
-
-    var containLabel = document.querySelectorAll('#contain-inner label');
-    var dexSwitchLabel = document.querySelectorAll('#dexswitch label');
-    var filterLabel = document.querySelectorAll('#filter-outer label');
-    for (var i = 0; i < containLabel.length; i++) {
-        containLabel[i].addEventListener("click", showChecked);
-    }
-    for (var i = 0; i < dexSwitchLabel.length; i++) {
-        dexSwitchLabel[i].addEventListener("click", showTotal);
-    }
-    for (var i = 0; i < filterLabel.length; i++) {
-        filterLabel[i].addEventListener("click", count);
-    }
-    document.getElementById('showall').addEventListener("click", count);
-
     function showChecked() {
         sleep(10).then(() => {
             document.querySelector('#count-current').innerText = document.querySelectorAll('#contain-inner div:not([style*="display: none"]):not(.hidden) input:checked').length;
@@ -29,6 +14,4 @@ function count() {
     showChecked();
     showTotal();
 }
-
-
 
