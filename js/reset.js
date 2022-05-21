fae862be2fd723f67cd132d12f975965ef35aa64
fixiie <warzhl0l@hotmail.com>
2022-05-21 04:41:26 +0200

    Update reset.js

--- js/reset.js
+++ js/reset.js
@@ -18,46 +18,15 @@ function modalReset() {
 
     function closeModal() {
 
-        var closeButton1 = document.getElementsByClassName('reset-modal-close1');
-        var closeButton2 = document.getElementsByClassName('reset-modal-close2');
-        var closeOverlay = document.getElementsByClassName('reset-modal-overlay');
+        var closeButton = document.getElementsByClassName('reset-modal-close');
 
-
-
-        for (var i = 0; i < closeButton1.length; i++) {
-            closeButton1[i].onclick = function() {
-                var modalWindow = this.parentNode.parentNode.parentNode;
+        for (var i = 0; i < closeButton.length; i++) {
+            closeButton[i].onclick = function() {
+                var modalWindowActive = document.querySelector(".reset-modal-outer.open");
 
                 $("body").removeClass("modal-open");
 
-                CheckAll();
-                count();
-
-                modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-            }
-        }
-
-        for (var i = 0; i < closeButton2.length; i++) {
-            closeButton2[i].onclick = function() {
-                var modalWindow = this.parentNode.parentNode.parentNode;
-
-                $("body").removeClass("modal-open");
-
-                UncheckAll();
-                count();
-
-                modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-            }
-        }
-
-
-        for (var i = 0; i < closeOverlay.length; i++) {
-            closeOverlay[i].onclick = function() {
-                var modalWindow = this.parentNode;
-
-                $("body").removeClass("modal-open");
-
-                modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+                modalWindowActive.classList ? modalWindowActive.classList.remove('open') : modalWindowActive.className = modalWindowActive.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
             }
         }
 
@@ -75,20 +44,6 @@ function modalReset() {
 
     ready(openModal);
     ready(closeModal);
-
-    function UncheckAll() {
-        var uncheck = document.querySelectorAll('#contain-inner div:not([style*="display: none"]):not(.hidden) input:checked');
-        for (var i = 0; i < uncheck.length; i++) {
-            uncheck[i].click();
-        }
-    }
-
-    function CheckAll() {
-        var check = document.querySelectorAll('#contain-inner div:not([style*="display: none"]):not(.hidden) input:not(:checked)');
-        for (var i = 0; i < check.length; i++) {
-            check[i].click();
-        }
-    }
 }
 
 
