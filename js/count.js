4c9f4e7d8a2a0911609f59c7a169d89403c8b82a
fixiie <warzhl0l@hotmail.com>
2022-05-26 08:11:14 +0200

    Update count.js

--- js/count.js
+++ js/count.js
@@ -1,13 +1,13 @@
 function count() {
     function showChecked() {
         sleep(10).then(() => {
-            document.querySelector('#count-current').innerText = document.querySelectorAll('#contain-inner div:not([style*="display: none"]):not(.hidden) input:checked').length;
+            document.querySelector('#count-current').innerText = document.querySelectorAll('#pokémon-outer > div li:not([style*="display: none"]):not(.hidden) input:checked').length;
         });
     }
 
     function showTotal() {
         sleep(10).then(() => {
-            document.querySelector('#count-total').innerText = document.querySelectorAll('#contain-inner div:not([style*="display: none"]):not(.hidden)').length;
+            document.querySelector('#count-total').innerText = document.querySelectorAll('#pokémon-outer > div li:not([style*="display: none"]):not(.hidden)').length;
         });
 
     }
