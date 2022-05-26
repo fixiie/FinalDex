ebef391a26bbbb2df309273a45dee6aa5b5a2105
fixiie <warzhl0l@hotmail.com>
2022-05-26 08:11:35 +0200

    Update resize.js

--- js/resize.js
+++ js/resize.js
@@ -2,7 +2,7 @@ const sliderDefaultValue = 300;
 const containMainDefaultDisplay = "block";
 const containImgDefaultHeight = "60%";
 const containImgDefaultMargin = "0 5%"
-const containNameDefaultDisplay = "flex";
+const containNameDefaultDisplay = "block";
 
 
 function resizeDiv() {
@@ -10,22 +10,20 @@ function resizeDiv() {
     var slider = document.getElementById('resize');
 	var output = document.getElementById("resize-value");
 
-    var u = document.querySelectorAll("#contain-inner div");
+    var lis = document.querySelectorAll("#pokémon-outer > div li");
 	
-	const main = document.querySelectorAll("#contain main");
-	const img = document.querySelectorAll(".contain-img");
-	const name = document.querySelectorAll(".contain-name");
+	const main = document.querySelectorAll("#pokémon-outer > div main:first-child");
+	const img = document.querySelectorAll("#pokémon-outer > div .contain-img");
+	const name = document.querySelectorAll("#pokémon-outer > div main:last-child");
 
 	var sliderSwitch = 180;
-    
-    for (i = 0; i < u.length; i++) {
-        u[i].style.width = slider.value + 'px';
-		u[i].style.height = slider.value + 'px';
-		u[i].style.fontSize = slider.value / 15 + 'px';
+
+    for (i = 0; i < lis.length; i++) {
+        lis[i].style.width = slider.value + 'px';
+		lis[i].style.height = slider.value + 'px';
+		lis[i].style.fontSize = slider.value / 15 + 'px';
 	
 		
-        
-        
         if (slider.value <= sliderSwitch) {
 			main[i].style.display = "none";
 			img[i].style.height = "90%";
