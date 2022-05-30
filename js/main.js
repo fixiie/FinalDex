1acd274269d5c71ececc2e193535ca653ddfae52
fixiie <warzhl0l@hotmail.com>
2022-05-30 16:52:17 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,166 @@
+(function() {
+
+    function openModal() {
+
+        var modalTrigger = document.getElementsByClassName('bug-modal-open');
+
+
+        for (var i = 0; i < modalTrigger.length; i++) {
+            modalTrigger[i].onclick = function() {
+                var modalWindow = document.getElementById("bug-modal");
+
+                $("body").addClass("modal-open");
+
+                modalWindow.classList ? modalWindow.classList.add('open') : modalWindow.className += ' ' + 'open';
+            }
+        }
+    }
+
+    function closeModal() {
+
+        var closeButton = document.getElementsByClassName('bug-modal-close');
+        var closeOverlay = document.getElementsByClassName('bug-modal-overlay');
+
+        for (var i = 0; i < closeButton.length; i++) {
+            closeButton[i].onclick = function() {
+                var modalWindow = this.parentNode.parentNode;
+
+                $("body").removeClass("modal-open");
+
+                modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+            }
+        }
+
+        for (var i = 0; i < closeOverlay.length; i++) {
+            closeOverlay[i].onclick = function() {
+                var modalWindow = this.parentNode;
+
+                $("body").removeClass("modal-open");
+
+                modalWindow.classList ? modalWindow.classList.remove('open') : modalWindow.className = modalWindow.className.replace(new RegExp('(^|\\b)' + 'open'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+            }
+        }
+
+    }
+
+
+    function ready(fn) {
+        if (document.readyState != 'loading') {
+            fn();
+        } else {
+            document.addEventListener('DOMContentLoaded', fn);
+        }
+    }
+
+
+    ready(openModal);
+    ready(closeModal);
+}());
+
+
+(function() {
+    // get all data in form and return object
+    function getFormData(form) {
+      var elements = form.elements;
+      var honeypot;
+  
+      var fields = Object.keys(elements).filter(function(k) {
+        if (elements[k].name === "honeypot") {
+          honeypot = elements[k].value;
+          return false;
+        }
+        return true;
+      }).map(function(k) {
+        if(elements[k].name !== undefined) {
+          return elements[k].name;
+        // special case for Edge's html collection
+        }else if(elements[k].length > 0){
+          return elements[k].item(0).name;
+        }
+      }).filter(function(item, pos, self) {
+        return self.indexOf(item) == pos && item;
+      });
+  
+      var formData = {};
+      fields.forEach(function(name){
+        var element = elements[name];
+        
+        // singular form elements just have one value
+        formData[name] = element.value;
+  
+        // when our element has multiple items, get their values
+        if (element.length) {
+          var data = [];
+          for (var i = 0; i < element.length; i++) {
+            var item = element.item(i);
+            if (item.checked || item.selected) {
+              data.push(item.value);
+            }
+          }
+          formData[name] = data.join(', ');
+        }
+      });
+  
+      // add form-specific values into the data
+      formData.formDataNameOrder = JSON.stringify(fields);
+      formData.formGoogleSheetName = form.dataset.sheet || "reports"; // default sheet name
+      formData.formGoogleSendEmail
+        = form.dataset.email || ""; // no email by default
+  
+      return {data: formData, honeypot: honeypot};
+    }
+  
+    function handleFormSubmit(event) {  // handles form submit without any jquery
+      event.preventDefault();           // we are submitting via xhr below
+      var form = event.target;
+      var formData = getFormData(form);
+      var data = formData.data;
+  
+      // If a honeypot field is filled, assume it was done so by a spam bot.
+      if (formData.honeypot) {
+        return false;
+      }
+  
+      disableAllButtons(form);
+      var url = form.action;
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', url);
+      // xhr.withCredentials = true;
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            form.reset();
+            var formElements = form.querySelector(".form-elements")
+            if (formElements) {
+              formElements.style.display = "none"; // hide form
+            }
+            var thankYouMessage = form.querySelector(".thankyou_message");
+            if (thankYouMessage) {
+              thankYouMessage.style.display = "flex";
+            }
+          }
+      };
+      // url encode form data for sending as post data
+      var encoded = Object.keys(data).map(function(k) {
+          return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+      }).join('&');
+      xhr.send(encoded);
+    }
+    
+    function loaded() {
+      // bind to the submit event of our form
+      var forms = document.querySelectorAll("form.gform");
+      for (var i = 0; i < forms.length; i++) {
+        forms[i].addEventListener("submit", handleFormSubmit, false);
+      }
+    };
+    document.addEventListener("DOMContentLoaded", loaded, false);
+  
+    function disableAllButtons(form) {
+      var buttons = form.querySelectorAll("button");
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+      }
+    }
+  })();
+  
