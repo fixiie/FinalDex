edabe000914da4c20c91a755ea759d41bbe9198a
fixiie <warzhl0l@hotmail.com>
2022-06-27 00:29:52 +0200

    Update ability.js

--- js/ability.js
+++ js/ability.js
@@ -58,37 +58,37 @@ var createAbility = function() {
 		var abilityAside4SidebarTitleLiTopText = document.createElement("h4");
 		abilityAside4SidebarTitleLiTop.appendChild(abilityAside4SidebarTitleLiTopText);
 		if(Generation <= 4) {
-			var sidebarAbilityList = ["Pokémon", "Primary Ability", "Secondary Ability", ];
+			var sidebarAbilityList = ["Pokémon","Primary Ability","Secondary Ability", ];
 		}
 		if(Generation >= 5) {
-			var sidebarAbilityList = ["Pokémon", "Primary Ability", "Secondary Ability", "Hidden Ability", ];
+			var sidebarAbilityList = ["Pokémon","Primary Ability","Secondary Ability","Hidden Ability", ];
 		}
 		for(var q = 0; q < sidebarAbilityList.length; q++) {
 			var abilityAside4SidebarTitleLiBottomText = document.createElement("h6");
 			abilityAside4SidebarTitleLiBottomText.innerText = sidebarAbilityList[q];
 			abilityAside4SidebarTitleLiBottom.appendChild(abilityAside4SidebarTitleLiBottomText);
 		}
-		abilityOuter.setAttribute("id", "ability-outer");
-		abilityOuter.setAttribute("name", "Abilities");
-		abilityAside1.setAttribute("id", "ability-aside1");
-		abilityAside1OptionsTitleOuter.setAttribute("id", "ability-options-title");
-		abilityAside1OptionsSearchOuter.setAttribute("id", "ability-options-search");
-		abilityAside1OptionsSearch.setAttribute("type", "text");
-		abilityAside1OptionsSearch.setAttribute("id", "ability-search");
-		abilityAside1OptionsSearch.setAttribute("placeholder", "Search Abilities...");
-		abilityAside1OptionsSearch.setAttribute("onfocus", "this.placeholder=''");
-		abilityAside1OptionsSearch.setAttribute("onblur", "this.placeholder='Search Abilities...'");
-		abilityAside1OptionsSearch.setAttribute("autocomplete", "off");
-		abilityAside1OptionsSearchExit.setAttribute("id", "ability-search-exit");
-		abilityAside1OptionsOuter.setAttribute("id", "ability-options-outer");
-		abilityAside1Options.setAttribute("id", "ability-options");
-		abilityAside2.setAttribute("id", "ability-aside2");
+		abilityOuter.setAttribute("id","ability-outer");
+		abilityOuter.setAttribute("name","Abilities");
+		abilityAside1.setAttribute("id","ability-aside1");
+		abilityAside1OptionsTitleOuter.setAttribute("id","ability-options-title");
+		abilityAside1OptionsSearchOuter.setAttribute("id","ability-options-search");
+		abilityAside1OptionsSearch.setAttribute("type","text");
+		abilityAside1OptionsSearch.setAttribute("id","ability-search");
+		abilityAside1OptionsSearch.setAttribute("placeholder","Search Abilities...");
+		abilityAside1OptionsSearch.setAttribute("onfocus","this.placeholder=''");
+		abilityAside1OptionsSearch.setAttribute("onblur","this.placeholder='Search Abilities...'");
+		abilityAside1OptionsSearch.setAttribute("autocomplete","off");
+		abilityAside1OptionsSearchExit.setAttribute("id","ability-search-exit");
+		abilityAside1OptionsOuter.setAttribute("id","ability-options-outer");
+		abilityAside1Options.setAttribute("id","ability-options");
+		abilityAside2.setAttribute("id","ability-aside2");
 		abilityAside2Title.classList.add("ability-title");
 		abilityAside2TitleName.innerText = "Abilities";
 		abilityAside2Debut.classList.add("ability-debut");
-		abilityAside3.setAttribute("id", "ability-aside3");
+		abilityAside3.setAttribute("id","ability-aside3");
 		abilityAside3Description.classList.add("ability-description");
-		abilityAside4.setAttribute("id", "ability-aside4");
+		abilityAside4.setAttribute("id","ability-aside4");
 		document.querySelector("#contain").appendChild(abilityOuter);
 		abilityOuter.appendChild(abilityAside1);
 		abilityAside1.appendChild(abilityAside1OptionsTitleOuter);
@@ -116,7 +116,7 @@ var createAbility = function() {
 
         var abilityAside2Game = document.createElement("span");
         var abilityAside2GameImage = document.createElement("img");
-        abilityAside2GameImage.src = "./media/Images/Misc/Title/Text/" + GameFullName.replaceAll(",", "").replaceAll("!", "").replaceAll("'", "").replaceAll(":", "") + ".png";
+        abilityAside2GameImage.src = "./media/Images/Misc/Title/Text/"+GameFullName.replaceAll(",","").replaceAll("!","").replaceAll("'","").replaceAll(":","")+".png";
         abilityAside2GameImage.setAttribute("onerror","this.display='none'");
         abilityAside2.appendChild(abilityAside2Game);
         abilityAside2Game.appendChild(abilityAside2GameImage);
@@ -125,18 +125,18 @@ var createAbility = function() {
 		var firstabilityiteration;
 		for(var q = 0; q < finaldataAbility.length; q++) {
 			for(var u = 0; u < finaldataAbility.length; u++) {
-				if(Object.keys(finaldataAbility[q])[u] == JSONPath_AbilityReference + "_" + "Name") {
+				if(Object.keys(finaldataAbility[q])[u] == JSONPath_AbilityReference+"_"+"Name") {
 					var abilityAside1OptionsInput = document.createElement("input");
 					var abilityAside1OptionsLabel = document.createElement("label");
-					abilityAside1OptionsInput.setAttribute("type", "radio");
-					abilityAside1OptionsInput.setAttribute("name", "ability-options");
-					abilityAside1OptionsInput.setAttribute("id", "ability-options-" + q);
-					abilityAside1OptionsInput.setAttribute("autocomplete", "off");
+					abilityAside1OptionsInput.setAttribute("type","radio");
+					abilityAside1OptionsInput.setAttribute("name","ability-options");
+					abilityAside1OptionsInput.setAttribute("id","ability-options-"+q);
+					abilityAside1OptionsInput.setAttribute("autocomplete","off");
 					abilityAside1OptionsInput.value = q;
-					abilityAside1OptionsLabel.setAttribute("for", "ability-options-" + q);
-					abilityAside1OptionsLabel.setAttribute("name", "medium");
-					abilityAside1OptionsLabel.setAttribute("data-search-name", finaldataAbility[q][JSONPath_AbilityReference + "_" + "Name"].toLowerCase());
-					abilityAside1OptionsLabel.innerText = finaldataAbility[q][JSONPath_AbilityReference + "_" + "Name"];
+					abilityAside1OptionsLabel.setAttribute("for","ability-options-"+q);
+					abilityAside1OptionsLabel.setAttribute("name","medium");
+					abilityAside1OptionsLabel.setAttribute("data-search-name", finaldataAbility[q][JSONPath_AbilityReference+"_"+"Name"].toLowerCase());
+					abilityAside1OptionsLabel.innerText = finaldataAbility[q][JSONPath_AbilityReference+"_"+"Name"];
 					abilityAside1Options.appendChild(abilityAside1OptionsInput);
 					abilityAside1Options.appendChild(abilityAside1OptionsLabel);
 					abilityAside1OptionsInput.addEventListener("click", abilityOptionsSelector);
@@ -177,39 +177,36 @@ var createAbility = function() {
 			i = this.value;
 		}
 	
-		abilityAside4SidebarTitleLiTopText.innerHTML = "Pokémon with&nbsp;" + "<u>" + finaldataAbility[i][JSONPath_AbilityReference + "_" + "Name"] + "</u>";
-		abilityAside2TitleID.innerText = "#" + getAbilityData(finaldataAbility[i][JSONPath_AbilityReference + "_" + "Name"], "ID");
-		abilityAside2DebutText.innerText = "Introduced in " + getAbilityData(finaldataAbility[i][JSONPath_AbilityReference + "_" + "Name"], "Debut");
-		abilityAside2TitleName.innerText = finaldataAbility[i][JSONPath_AbilityReference + "_" + "Name"];
-		abilityAside3DescriptionText.innerText = getAbilityData(finaldataAbility[i][JSONPath_AbilityReference + "_" + "Name"], "Flavor");
+		abilityAside4SidebarTitleLiTopText.innerHTML = "Pokémon with&nbsp;"+"<u>"+finaldataAbility[i][JSONPath_AbilityReference+"_"+"Name"]+"</u>";
+		abilityAside2TitleID.innerText = "#"+getAbilityData(finaldataAbility[i][JSONPath_AbilityReference+"_"+"Name"], "ID");
+		abilityAside2DebutText.innerText = "Introduced in "+getAbilityData(finaldataAbility[i][JSONPath_AbilityReference+"_"+"Name"], "Debut");
+		abilityAside2TitleName.innerText = finaldataAbility[i][JSONPath_AbilityReference+"_"+"Name"];
+		abilityAside3DescriptionText.innerText = getAbilityData(finaldataAbility[i][JSONPath_AbilityReference+"_"+"Name"], "Flavor");
 		var lis = document.querySelectorAll("#ability-aside4 ul li");
 		for(var q = 0; q < lis.length; q++) {
 			lis[q].remove();
 		}
 
 
-		var sidebarAbilityListFull = sidebarAbilityList.map((v) => v + "_" + JSONPath_Ability);sidebarAbilityListFull = sidebarAbilityListFull.filter((item) => item !== "Pokémon" + "_" + JSONPath_Ability);
+		var sidebarAbilityListFull = sidebarAbilityList.map((v) => v+"_"+JSONPath_Ability);sidebarAbilityListFull = sidebarAbilityListFull.filter((item) => item !== "Pokémon"+"_"+JSONPath_Ability);
 		for(var q = 0; q < sidebarAbilityListFull.length; q++) {
-			sidebarAbilityListFull[q] = sidebarAbilityListFull[q].replaceAll(" Ability", "");
+			sidebarAbilityListFull[q] = sidebarAbilityListFull[q].replaceAll(" Ability","");
 		}
-		var AbilityResults = getPokémonData(finaldataPokémonAbility, finaldataAbility[i][JSONPath_AbilityReference+"_" + "Name"], sidebarAbilityListFull);
+		var AbilityResults = getPokémonData(finaldataPokémonAbility, finaldataAbility[i][JSONPath_AbilityReference+"_"+"Name"], sidebarAbilityListFull);
 	
 		for(var q = 0; q < AbilityResults.length; q++) {
 			var abilityAside4SidebarLi = document.createElement("li");
 			abilityAside4SidebarUl.appendChild(abilityAside4SidebarLi);
 			for(var u = 0; u < sidebarAbilityList.length; u++) {
+				var int = AbilityResults[q]["Integer"];
+				var name = getPokémonName(int);
 				if(u == 0) {
 					var abilityAside4SidebarLiImgOuter = document.createElement("div");
 					var abilityAside4SidebarLiImg = document.createElement("img");
-					abilityAside4SidebarLiImg.src = "./media/Images/Pokémon/Box/PNG/" + MEDIAPath_Pokémon_Box + "/" + AbilityResults[q]["Folder"] + AbilityResults[q]["File"] + ".png";
-					abilityAside4SidebarLiImg.setAttribute("onerror", "this.src='./media/Images/Pokémon/Box/PNG/"+MEDIAPath_Pokémon_Box+"/0.png'");
-					if(AbilityResults[q]["Form"] != undefined) {
-						abilityAside4SidebarLiImg.setAttribute("title", AbilityResults[q]["Form"]);
-						abilityAside4SidebarLiImg.setAttribute("value",getPokémonInt(AbilityResults[q]["Form"]));
-					} else {
-						abilityAside4SidebarLiImg.setAttribute("title", AbilityResults[q]["Pokémon"]);
-						abilityAside4SidebarLiImg.setAttribute("value",getPokémonInt(AbilityResults[q]["Pokémon"]));
-					}
+					abilityAside4SidebarLiImg.src = "./media/Images/Pokémon/Box/PNG/"+MEDIAPath_Pokémon_Box+"/"+getPokémonMediaPath(int,"Box")+".png";
+					abilityAside4SidebarLiImg.setAttribute("title", name);
+					abilityAside4SidebarLiImg.setAttribute("value",int);
+					abilityAside4SidebarLiImg.setAttribute("onerror","this.src='./media/Images/Pokémon/Box/PNG/"+MEDIAPath_Pokémon_Box+"/0.png'");
 					abilityAside4SidebarLi.appendChild(abilityAside4SidebarLiImgOuter);
 					abilityAside4SidebarLiImgOuter.appendChild(abilityAside4SidebarLiImg);
 					abilityAside4SidebarLiImg.addEventListener("click", modalData);
@@ -219,12 +216,12 @@ var createAbility = function() {
 					if(AbilityResults[q][sidebarAbilityListFull[u - 1]] != undefined) {
 						abilityAside4SidebarLiText.innerText = AbilityResults[q][sidebarAbilityListFull[u - 1]];
 					}
-					if(AbilityResults[q][sidebarAbilityListFull[u - 1]] == finaldataAbility[i][JSONPath_AbilityReference + "_" + "Name"]) {
+					if(AbilityResults[q][sidebarAbilityListFull[u - 1]] == finaldataAbility[i][JSONPath_AbilityReference+"_"+"Name"]) {
 						abilityAside4SidebarLiText.classList.add("active");
 					} else {
 						abilityAside4SidebarLiText.addEventListener("click", dataRedirect);
 					}
-					abilityAside4SidebarLiText.setAttribute("name", "ability");
+					abilityAside4SidebarLiText.setAttribute("name","ability");
 					abilityAside4SidebarLiText.setAttribute("title", sidebarAbilityList[u]);
 					abilityAside4SidebarLi.appendChild(abilityAside4SidebarLiTextOuter);
 					abilityAside4SidebarLiTextOuter.appendChild(abilityAside4SidebarLiText);
